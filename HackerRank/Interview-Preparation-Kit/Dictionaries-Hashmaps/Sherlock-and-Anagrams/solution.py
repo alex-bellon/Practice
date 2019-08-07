5275203f1188c67ede228caf34eaec6835f8acd1
alex-bellon <alexrbellon@gmail.com>
2019-08-06 21:19:54 -0700

    More work on Sherlock

--- HackerRank/Interview-Preparation-Kit/Dictionaries-Hashmaps/Sherlock-and-Anagrams/solution.py
+++ HackerRank/Interview-Preparation-Kit/Dictionaries-Hashmaps/Sherlock-and-Anagrams/solution.py
@@ -8,7 +8,21 @@ import sys
 
 # Complete the sherlockAndAnagrams function below.
 def sherlockAndAnagrams(s):
-    for i in range(1, len(s) - 1):
+    pairs = 0
+    for i in range(1, len(s)):
+        counts = dict()
+        subs = list()
+        for j in range(0, len(s) - i + 1):
+            sub = list(s[j:j+i])
+            sub.sort()
+            sub = str(sub)
+            if not sub in counts:
+                counts[sub] = 0
+            counts[sub] += 1
+        for sub in counts:
+            if not counts[sub] % 2:
+                pairs += counts[sub] // 2
+    return pairs # This undercounts if there's something like aaaa
 
         letters = list(s[])
 if __name__ == '__main__':
